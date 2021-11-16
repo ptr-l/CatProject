8dc6920833095b8b8b9ce6acf133a9a1cff27cac
Peter Lamson <ptrlmsn@gmail.com>
2021-11-16 13:32:10 -0500

    Incomplete Todo List Added to JS File

---
+++ SRC/index.js
@@ -0,0 +1,9 @@
+//JAVASCRIPT TODO:
+//On Page Load - Display Cat Images from API (fetch->render)
+// Peter Note: It looks like the API doesn't hard limit the # of cats you can grab at once - you have to set your own return limiters in your request. Should decide how many cats to take, probably based on webpage design?
+//Allow Rating Beteween 1-5 on any of the loaded cat pictures - Submit Via Clicking Number next to Img (Peter: If I'm understanding the proposed design properly)
+//Submittable Comment form that will render the comment on the webpage after its submitted + clear the form - 
+//      Peter: Is there a way to only have one comment form that can work for every image - or will we have a seperate comment form for each image that loads on the page?
+//Stretch: On Submitting Tag- Display Cats with associated submitted tag
+//  Stretch for the Stretch: Offer an expandable list of tags so people know what options they can choose from.
+//
